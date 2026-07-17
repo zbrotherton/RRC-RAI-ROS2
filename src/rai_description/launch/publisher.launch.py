431cc9987b9202aedf1e78433e56d43a66845678
Zane Brotherton <zanebrotherton@gmail.com>
2026-07-17 14:12:37 +0000

    add launch files

---
+++ src/rai_description/launch/publisher.launch.py
@@ -0,0 +1,106 @@
+import os
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.conditions import IfCondition, UnlessCondition
+from launch.substitutions import Command, LaunchConfiguration
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+def generate_launch_description():
+
+  # Set the path to this package.
+  rai_description_package  = FindPackageShare(package='rai_description').find('rai_description')
+
+  # Set the path to the URDF file
+  default_urdf_model_path = os.path.join(rai_description_package, 'urdf/rai.urdf.xacro')
+
+  # Launch configuration variables specific to simulation
+  gui = LaunchConfiguration('gui')
+  urdf_model = LaunchConfiguration('urdf_model')
+  use_robot_state_pub = LaunchConfiguration('use_robot_state_pub')
+  use_sim_time = LaunchConfiguration('use_sim_time')
+
+  # Declare the launch arguments  
+  declare_urdf_model_path_cmd = DeclareLaunchArgument(
+    name='urdf_model', 
+    default_value=default_urdf_model_path, 
+    description='Absolute path to robot urdf file')
+  
+  declare_use_joint_state_publisher_cmd = DeclareLaunchArgument(
+    name='gui',
+    default_value='False',
+    description='Flag to enable joint_state_publisher_gui')
+  
+  declare_use_robot_state_pub_cmd = DeclareLaunchArgument(
+    name='use_robot_state_pub',
+    default_value='True',
+    description='Whether to start the robot state publisher')
+
+  declare_use_sim_time_cmd = DeclareLaunchArgument(
+    name='use_sim_time',
+    default_value='False',
+    description='Use simulation (Gazebo) clock if true')
+   
+  # Specify the actions
+  
+  # Publish the joint state values for the non-fixed joints in the URDF file.
+  start_joint_state_publisher_cmd = Node(
+    condition=UnlessCondition(gui),
+    package='joint_state_publisher',
+    executable='joint_state_publisher',
+    name='joint_state_publisher')
+  
+  # A GUI to manipulate the joint state values
+  start_joint_state_publisher_gui_node = Node(
+    condition=IfCondition(gui),
+    package='joint_state_publisher_gui',
+    executable='joint_state_publisher_gui',
+    name='joint_state_publisher_gui')
+  # Subscribe to the joint states of the robot, and publish the 3D pose of each link.
+  start_robot_state_publisher_cmd = Node(
+    condition=IfCondition(use_robot_state_pub),
+    package='robot_state_publisher',
+    executable='robot_state_publisher',
+    parameters=[{'use_sim_time': use_sim_time, 
+    'robot_description': Command(
+      [
+        'xacro ', urdf_model,
+        ' use_mock_hardware:=', LaunchConfiguration('use_mock_hardware')
+       ]
+      ),
+    }],
+    )
+  
+  # Create foxglove bridge
+  start_foxglove_bridge_cmd = Node(
+    package='foxglove_bridge',
+    executable='foxglove_bridge'
+  )
+
+  # Create the launch description and populate
+  # Create the launch description and populate
+  launch_description = LaunchDescription(
+    [
+        DeclareLaunchArgument(
+            'use_mock_hardware',
+            default_value='false',
+            description='Run in Simulation'
+        ),
+
+    ]
+  )
+
+
+  # Declare the launch options
+  launch_description.add_action(declare_urdf_model_path_cmd)
+  launch_description.add_action(declare_use_joint_state_publisher_cmd)
+  launch_description.add_action(declare_use_robot_state_pub_cmd)  
+  launch_description.add_action(declare_use_sim_time_cmd)
+
+  # Add any actions
+  launch_description.add_action(start_foxglove_bridge_cmd)
+  launch_description.add_action(start_joint_state_publisher_cmd)
+  launch_description.add_action(start_joint_state_publisher_gui_node)
+  launch_description.add_action(start_robot_state_publisher_cmd)
+
+  return launch_description
