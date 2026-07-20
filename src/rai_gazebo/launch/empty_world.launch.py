fd9a7d99fbd1c23c6a9327832c8d20f037fd2cad
Zane Brotherton <zanebrotherton@gmail.com>
2026-07-20 03:58:16 +0000

    locate STLs correctly in launch

--- src/rai_gazebo/launch/empty_world.launch.py
+++ src/rai_gazebo/launch/empty_world.launch.py
@@ -3,15 +3,15 @@ import os
 from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument, SetEnvironmentVariable
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
-from launch_ros.substitutions import FindPackageShare
-
 from launch_ros.actions import Node
 
 def generate_launch_description():
     ros_gz_sim_package = get_package_share_directory('ros_gz_sim')
+    rai_description_package = os.path.realpath(os.path.join(get_package_share_directory('rai_description'), ".."))
+    print(rai_description_package)
 
     gz_launch_path = os.path.join(ros_gz_sim_package, 'launch', 'gz_sim.launch.py')
 
@@ -32,11 +32,16 @@ def generate_launch_description():
     gazebo = IncludeLaunchDescription(
             PythonLaunchDescriptionSource(gz_launch_path),
             launch_arguments={'gz_args': ['-r -v4 ', world], 'on_exit_shutdown': 'true'}.items()
-        )
+    )
+    
+    set_resource_env = SetEnvironmentVariable(
+        name='GZ_SIM_RESOURCE_PATH',
+        value=rai_description_package
+    )
 
     spawn_entity = Node(package='ros_gz_sim', executable='create',
         arguments=['-topic', 'robot_description',
-                    '-name', 'rai_robot'],
+                    '-name', 'rai'],
         output='screen'
     )
 
@@ -53,6 +58,7 @@ def generate_launch_description():
     )
     
     return LaunchDescription([
+        set_resource_env,
         world_arg,
         gazebo,
         spawn_entity,
