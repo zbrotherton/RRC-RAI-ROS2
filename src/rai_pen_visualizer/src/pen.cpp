fc876bc0d3329fe13d72a11b3cfd7eac31bf4917
zbrotherton <zanebrotherton@gmail.com>
2026-08-02 19:31:13 +0000

    Alter pen marker size and type

--- src/rai_pen_visualizer/src/pen.cpp
+++ src/rai_pen_visualizer/src/pen.cpp
@@ -28,28 +28,27 @@ std::optional<visualization_msgs::msg::Marker> Pen::generate_marker(unsigned int
     last_transform_stamp_ = t.header.stamp;
 
     visualization_msgs::msg::Marker marker;
-
     marker.header.frame_id = canvas_frame_id_;
     marker.header.stamp = clock_->now();
 
     marker.ns = "pen_visualizer";
     marker.id = marker_count;
 
-    marker.type = visualization_msgs::msg::Marker::POINTS;
+    marker.type = visualization_msgs::msg::Marker::CUBE;
 
     marker.action = visualization_msgs::msg::Marker::ADD;
 
     marker.pose.position.x = t.transform.translation.x;
     marker.pose.position.y = t.transform.translation.y;
-    marker.pose.position.z = t.transform.translation.z;
+    marker.pose.position.z = -0.01;
     marker.pose.orientation.x = t.transform.rotation.x;
     marker.pose.orientation.y = t.transform.rotation.y;
     marker.pose.orientation.z = t.transform.rotation.z;
     marker.pose.orientation.w = t.transform.rotation.w;
 
-    marker.scale.x = 1.0;
-    marker.scale.y = 1.0;
-    marker.scale.z = 1.0;
+    marker.scale.x = 0.001;
+    marker.scale.y = 0.001;
+    marker.scale.z = 0.001;
 
     marker.color.r = 0.0f;
     marker.color.g = 1.0f;
